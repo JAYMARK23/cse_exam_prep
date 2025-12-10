bb691d75fb96fb31441b7a67f27c0a031198d725
Kien <kienztalaogon@gmail.com>
2025-12-10 09:58:19 +0800

    chore(android): configure Firebase android package & gradle

---
+++ mobile_flutter/android/app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.restocknroll.mobile"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // Set the Application ID to match Firebase package name
+        applicationId = "com.restocknroll.mobile"
+        // Ensure minSdk is at least 21 for Firebase
+        minSdk = 21
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+// Apply Google Services plugin to enable reading google-services.json
+apply(plugin = "com.google.gms.google-services")
---
+++ mobile_flutter/android/app/src/main/kotlin/com/example/restock_n_roll_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.restocknroll.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
